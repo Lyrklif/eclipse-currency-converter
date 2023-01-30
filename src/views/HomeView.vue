c1017483ef9d33b602a03edb40f4397f2579c28c
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-30 17:17:49 +0600

    feat: вывод списка валютных пар в таблицу

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,29 @@
+<script lang="ts" setup>
+import { useCurrenciesStore } from "../stores/currencies";
+
+const store = useCurrenciesStore();
+</script>
+
 <template>
-  <div>Main page</div>
+  <table>
+    <thead>
+      <tr>
+        <th>CharCode</th>
+        <th>Nominal</th>
+        <th>Name</th>
+        <th>Value</th>
+        <th>Previous</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(value, key) in store.currencyList" :key="key">
+        <td>{{ value.CharCode }}</td>
+        <td>{{ value.Nominal }}</td>
+        <td>{{ value.Name }}</td>
+        <td>{{ value.Value }}</td>
+        <td>{{ value.Previous }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
