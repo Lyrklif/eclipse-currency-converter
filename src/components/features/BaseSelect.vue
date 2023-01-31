bb0536b63e017e2a3edf373b8cd52ffc2785518a
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-31 20:15:07 +0600

    feat: добавление компонента BaseSelect

---
+++ src/components/features/BaseSelect.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+import { ref } from "vue";
+
+const emit = defineEmits(["change"]);
+const selected = ref<string>("");
+
+defineProps({
+  value: { type: String as PropType<string>, required: true },
+  options: { type: Array as PropType<Array<string>>, required: true },
+});
+
+const changeHandler = (e: any) => {
+  emit("change", e.target.value);
+};
+</script>
+
+<template>
+  <select v-model="selected" :value="value" @change="changeHandler">
+    <option v-for="name in options" :key="`select-${name}`" :value="name">
+      {{ name }}
+    </option>
+  </select>
+</template>
