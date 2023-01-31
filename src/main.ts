8bfad53c0e12fad8b283e6749b9e45e7720da63e
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-31 22:38:59 +0600

    feat: добавление normalize.css

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import filters from "./helpers/filters";
-
+import "normalize.css";
 
 const app = createApp(App);
 
