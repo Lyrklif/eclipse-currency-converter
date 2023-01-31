46f916aa55b96bdb3bdb24d21e9b6b637f0638d7
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-31 20:16:55 +0600

    feat: добавление конвертации валют

---
+++ src/stores/converter.ts
@@ -0,0 +1,55 @@
+import { defineStore } from "pinia";
+import type { RemovableRef } from "@vueuse/core";
+import { ref, computed } from "vue";
+
+const START_VALUE = 0;
+
+export interface StoreInterface {
+  baseValue: RemovableRef<number>;
+  calculateValue: RemovableRef<number>;
+  setBaseValue: (value: number) => void;
+  switchCurrencies: () => void;
+  setFrom: (value: string) => void;
+  setTo: (value: string) => void;
+}
+
+export const useConverterStore = defineStore(
+  "converter",
+  (): StoreInterface => {
+    const baseValue = ref<number>(START_VALUE);
+    const from = ref<string>("RUB");
+    const to = ref<string>("USD");
+
+    const calculateValue = computed(() => {
+      // @ts-ignore
+      return window.fx(baseValue.value).from(from.value).to(to.value);
+    });
+
+    function switchCurrencies() {
+      const oldFrom = from.value;
+      const oldTo = to.value;
+
+      from.value = oldTo;
+      to.value = oldFrom;
+    }
+
+    function setBaseValue(value: number) {
+      baseValue.value = value || 0;
+    }
+    function setFrom(currencyName: string) {
+      from.value = currencyName;
+    }
+    function setTo(currencyName: string) {
+      to.value = currencyName;
+    }
+
+    return {
+      baseValue,
+      calculateValue,
+      setBaseValue,
+      switchCurrencies,
+      setFrom,
+      setTo,
+    };
+  }
+);
--- src/views/ConverterView.vue
+++ src/views/ConverterView.vue
@@ -1,10 +1,45 @@
 <script lang="ts" setup>
-import CurrencyChanger from "../components/features/CurrencyChanger.vue";
+import { useConverterStore } from "../stores/converter";
 import { useCurrenciesStore } from "../stores/currencies";
+import BaseSelect from "../components/features/BaseSelect.vue";
+import { ref } from "vue";
 
-const store = useCurrenciesStore();
+const convertStore = useConverterStore();
+const currenciesStore = useCurrenciesStore();
+const value = ref(convertStore.baseValue);
+
+const changeValue = (e: any) => {
+  convertStore.setBaseValue(e.target.value);
+};
 </script>
 
 <template>
-  <div>Converter page</div>
+  <main>
+    <input
+      type="number"
+      inputmode="numeric"
+      v-model.number="value"
+      @input="changeValue"
+      min="0"
+    />
+    <BaseSelect
+      :value="convertStore.from"
+      :options="currenciesStore.currencyList"
+      @change="convertStore.setFrom"
+    />
+
+    <button @click="convertStore.switchCurrencies">{{ "<=>" }}</button>
+
+    <input
+      type="number"
+      inputmode="numeric"
+      disabled
+      v-model.number="convertStore.calculateValue"
+    />
+    <BaseSelect
+      :value="convertStore.to"
+      :options="currenciesStore.currencyList"
+      @change="convertStore.setTo"
+    />
+  </main>
 </template>
