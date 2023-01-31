f5f34ded83d46f41947a25dba3321b47657880f3
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-31 13:21:18 +0600

    feat: добавление страницы конвертации валюты

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,20 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
+import ConverterView from "../views/ConverterView.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/converter",
+      name: "converter",
+      component: () => import("../views/ConverterView.vue"),
     },
   ],
 });
---
+++ src/views/ConverterView.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import CurrencyChanger from "../components/features/CurrencyChanger.vue";
+import { useCurrenciesStore } from "../stores/currencies";
+
+const store = useCurrenciesStore();
+</script>
+
+<template>
+  <div>Converter page</div>
+</template>
