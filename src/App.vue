40fbe333912a6aa53a52cab105a7bdeb71c141ed
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-31 20:15:38 +0600

    feat: отображать данные после загрузки

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { RouterView } from "vue-router";
 import AppHeader from "./components/layout/header/AppHeader.vue";
 import AppFooter from "./components/layout/footer/AppFooter.vue";
 import { useCurrenciesStore } from "./stores/currencies";
 
 const store = useCurrenciesStore();
-store.loadCurrencies();
-store.loadRates();
+const isLoading = ref<boolean>(true);
+const isError = ref<boolean>(false);
+
+Promise.all([store.loadCurrencies(), store.loadRates()])
+  .catch(() => {
+    isError.value = true;
+  })
+  .finally(() => {
+    isLoading.value = false;
+  });
 </script>
 
 <template>
   <div>
-    <div>
-      <AppHeader />
+    <AppHeader />
 
-      <main>
-        <RouterView />
-      </main>
-    </div>
+    <p v-if="isLoading">Loading...</p>
+    <p v-else-if="isError">
+      Не удалось загрузить данные. Попробуйте ещё раз через некоторое время
+    </p>
+    <RouterView v-else />
 
     <AppFooter />
   </div>
