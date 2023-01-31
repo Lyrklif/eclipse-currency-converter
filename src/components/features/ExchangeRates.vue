dc20597364437a6ecef5994d612b8fd671d47b22
Alexandr Kochurov <b2b4ly@gmail.com>
2023-01-31 17:24:53 +0600

    refactor: отдельный компонент ExchangeRates

---
+++ src/components/features/ExchangeRates.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import ValueCell from "./ValueCell.vue";
+import { useCurrenciesStore } from "../../stores/currencies";
+
+const store = useCurrenciesStore();
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>CharCode</th>
+        <th>Nominal</th>
+        <th>Name</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(value, key) in store.exchangeRates" :key="key">
+        <td>{{ value.CharCode }}</td>
+        <td>{{ value.Nominal }}</td>
+        <td>{{ value.Name }}</td>
+        <ValueCell :current="value.Value" :previous="value.Previous" />
+      </tr>
+    </tbody>
+  </table>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,11 @@
 <script lang="ts" setup>
 import CurrencyChanger from "../components/features/CurrencyChanger.vue";
-import { useCurrenciesStore } from "../stores/currencies";
-
-const store = useCurrenciesStore();
+import ExchangeRates from "../components/features/ExchangeRates.vue";
 </script>
 
 <template>
   <div>
     <CurrencyChanger />
-
-    <table>
-      <thead>
-        <tr>
-          <th>CharCode</th>
-          <th>Nominal</th>
-          <th>Name</th>
-          <th>Value</th>
-          <th>Previous</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="(value, key) in store.exchangeRates" :key="key">
-          <td>{{ value.CharCode }}</td>
-          <td>{{ value.Nominal }}</td>
-          <td>{{ value.Name }}</td>
-          <td>{{ value.Value }}</td>
-          <td>{{ value.Previous }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <ExchangeRates />
   </div>
 </template>
