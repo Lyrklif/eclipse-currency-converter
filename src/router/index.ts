3f81f3b7e036b2bdc7e1ec0df45ca4580e7d32f9
Alexandr Kochurov <b2b4ly@gmail.com>
2023-02-25 23:44:36 +0600

    refactor: delete unnecessary imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-
 import HomeView from "../views/HomeView.vue";
-import ConverterView from "../views/ConverterView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
