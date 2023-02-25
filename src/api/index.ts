a549b574ca29e29d51f33b2bebce809bc90db08b
Alexandr Kochurov <b2b4ly@gmail.com>
2023-02-25 23:43:51 +0600

    refactor: rename variables

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,7 +11,7 @@ const instance: AxiosInstance = axios.create({
   baseURL: "https://www.cbr-xml-daily.ru/",
 });
 
-const API: ApiInterface = {
+const apiClient: ApiInterface = {
   exchanges() {
     return instance.get("daily_json.js");
   },
@@ -20,4 +20,4 @@ const API: ApiInterface = {
   },
 };
 
-export default API;
+export default apiClient;
